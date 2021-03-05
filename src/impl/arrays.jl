ab6b1dec22a17ee3a23ccc6b003a60bd37ef7a30
Takafumi Arakaki <aka.tkf@gmail.com>
2021-03-05 15:14:30 -0800

    Support Julia 1.6 (#5)
    * Fix a bug caught in Julia 1.6
    
    * Don't use invoke
    
    * Use Setfield instead of Accessors for older Julia

--- src/impl/arrays.jl
+++ src/impl/arrays.jl
@@ -3,15 +3,23 @@ struct UnionArray{T, N, P} <: Abstract.UnionArray{T, N}
 
     function UnionArray(parent::P) where {T, N, P <: AbstractArray{T, N}}
         # Make sure that `A.parent` is not a UnionArray/Vector
-        P <: UnionArrayImpls && throw(MethodError(UnionArray, (A,)))
+        P <: UnionArrayImpls && throw(MethodError(UnionArray, (parent,)))
         return new{T, N, P}(parent)
     end
 end
 
 const UnionArrayImpls = Union{UnionVector, UnionArray}
 
-default_reshape(A::UnionVector, dims::T) where T =
-    invoke(reshape, Tuple{AbstractArray, T}, A, dims)
+# A very minimal dummy array implementation just for implementing `default_reshape`:
+struct DummyArray{N} <: AbstractArray{Any,N}
+    dims::NTuple{N,Int}
+end
+Base.size(A::DummyArray) = A.dims
+
+function default_reshape(A::UnionVector, dims::T) where {T}
+    dummy = reshape(DummyArray(size(A)), dims)
+    return @set dummy.parent = A
+end
 
 ua_reshape(A::UnionVector, dims) = UnionArray(default_reshape(A, dims))
 ua_reshape(A::UnionArray, dims) = UnionArray(reshape(A.parent, dims))
--- src/impl/impl.jl
+++ src/impl/impl.jl
@@ -3,6 +3,7 @@ module Impl
 using Base: Dims
 using Transducers
 using Transducers: @return_if_reduced, next, complete
+using Setfield: @set  # using Setfield instead of Accessors for older Julia
 
 using ..Abstract
 
