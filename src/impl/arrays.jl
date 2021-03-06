7a67397ff36ad645789fd8ed43827700095f1a9a
Takafumi Arakaki <aka.tkf@gmail.com>
2021-03-05 19:57:29 -0500

    CUDA support for non-vector UnionArray

--- src/impl/arrays.jl
+++ src/impl/arrays.jl
@@ -10,6 +10,18 @@ end
 
 const UnionArrayImpls = Union{UnionVector, UnionArray}
 
+function Adapt.adapt_structure(to, A::UnionArray)
+    B = Adapt.adapt(to, parent(A))
+    if B isa UnionArray
+        # `Adapt.adapt` might just call `reshape`, which then wraps the array
+        # to `UnionArray`. In this case, there is no need to re-wrap it:
+        return B
+    else
+        # Maybe just throw?
+        return UnionArray(B)
+    end
+end
+
 # A very minimal dummy array implementation just for implementing `default_reshape`:
 struct DummyArray{N} <: AbstractArray{Any,N}
     dims::NTuple{N,Int}
@@ -45,3 +57,31 @@ Base.setindex!(A::UnionArray, v, I...) = setindex!(A.parent, v, I...)
 
 # Base.showarg(io::IO, A::UnionArray, toplevel) =
 #     Base.showarg(io, A.parent, toplevel)
+
+Abstract.UnionArray(
+    undef::UndefInitializer,
+    VectorType::Type{<:AbstractVector},
+    ETS::ElTypeSpec,
+    dims::Union{Integer,Tuple{Vararg{Integer}}},
+) = Abstract.UnionArray(undef, VectorType, VectorType{UInt8}, ETS, dims)
+
+Abstract.UnionArray(
+    undef::UndefInitializer,
+    DataVectorType::Type{<:AbstractVector},
+    TypeTagVectorType::Type{<:AbstractVector{UInt8}},
+    ETS::ElTypeSpec,
+    n::Union{Integer,Tuple{Integer}},
+) = UnionVector(undef, DataVectorType, TypeTagVectorType, ETS, first(n))
+
+Abstract.UnionArray(
+    undef::UndefInitializer,
+    DataVectorType::Type{<:AbstractVector},
+    TypeTagVectorType::Type{<:AbstractVector{UInt8}},
+    ETS::ElTypeSpec,
+    dims::Tuple{Integer,Integer,Vararg{Integer}},
+) = reshape(UnionVector(undef, DataVectorType, TypeTagVectorType, ETS, prod(dims)), dims)
+
+function Base.fill!(A::UnionArray, x)
+    fill!(parent(parent(A)), x)
+    return A
+end
--- test/cuda/test_folds.jl
+++ test/cuda/test_folds.jl
@@ -6,7 +6,7 @@ using FoldsCUDA
 using Test
 using UnionArrays
 
-@testset begin
+@testset "UnionVector" begin
     xs = UnionVector(undef, CuVector, (Float32, Missing), 3)
     fill!(xs, 1)
     CUDA.@allowscalar xs[2] = missing
@@ -14,6 +14,16 @@ using UnionArrays
     @test (CUDA.@allowscalar xs[2]) === missing
     @test (CUDA.@allowscalar xs[3]) === 1.0f0
     @test Folds.sum(x -> x isa Missing ? 0.0f0 : x, xs, CUDAEx()) == 2
+end
+
+@testset "UnionMatrix" begin
+    A = UnionArray(undef, CuVector, (Float32, Missing), (3, 2))
+    fill!(A, 1)
+    CUDA.@allowscalar A[2, 2] = missing
+    @test (CUDA.@allowscalar A[1, 1]) === 1.0f0
+    @test (CUDA.@allowscalar A[2, 2]) === missing
+    @test (CUDA.@allowscalar A[3, 2]) === 1.0f0
+    @test Folds.sum(x -> x isa Missing ? 0.0f0 : x, A, CUDAEx()) == 5
 end
 
 end  # module
