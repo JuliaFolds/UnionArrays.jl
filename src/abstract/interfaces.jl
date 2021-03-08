c5b968e4c6d0d98aafe244d6bdfad631dbb99a23
Takafumi Arakaki <aka.tkf@gmail.com>
2021-03-08 14:06:34 -0500

    Make collect(A::UnionArray) look more correct

--- src/abstract/interfaces.jl
+++ src/abstract/interfaces.jl
@@ -3,4 +3,4 @@ using Adapt: adapt
 default_collect(A::AbstractArray{T,N}) where {T,N} =
     invoke(collect, Tuple{AbstractArray{T,N}}, A)
 
-Base.collect(A::UnionArray) = default_collect(adapt(Vector, A))
+Base.collect(A::UnionArray) = default_collect(adapt(Array, A))
