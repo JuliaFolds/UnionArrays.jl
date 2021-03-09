05fa59450575eeb7cac7620bdbc6071a9f76a979
Takafumi Arakaki <aka.tkf@gmail.com>
2021-03-08 19:47:00 -0500

    Hack LOAD_PATH to workaround a Julia 1.6 bug

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,16 @@
 module TestUnionArrays
 using Test
 
+if VERSION ≥ v"1.6-"
+    try
+        pkgid = Base.PkgId(Base.UUID("052768ef-5323-5732-b1bb-66c8b64840ba"), "CUDA")
+        Base.require(pkgid)
+    catch
+        @info "Failed to import CUDA. Trying again with `@stdlib`..."
+        push!(LOAD_PATH, "@stdlib")
+    end
+end
+
 const TEST_CUDA = try
     import CUDA
     CUDA.has_cuda_gpu()
